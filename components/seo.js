b443e5fa4a00b11ba070772619f168d082feafef
Huelder <huelderantuness@gmail.com>
2020-12-18 15:28:14 -0300

    mod: better seo

--- components/seo.js
+++ components/seo.js
@@ -3,6 +3,19 @@ import Head from 'next/head'
 export default function SEO ({ title, description, keywords }) {
   return (
     <Head>
+      <script
+        async
+        src='https://www.googletagmanager.com/gtag/js?id=G-W4T7B1N0XJ'
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-W4T7B1N0XJ');`
+        }}
+      ></script>
       <title>{title}</title>
 
       <meta httpEquiv='content-language' content='pt-br' />
